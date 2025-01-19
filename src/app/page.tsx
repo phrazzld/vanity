22ecc61809851130e6dbd58af7f548203a330bc4
Phaedrus Raznikov <phaedrus.raznikov@memre.ai>
2025-01-18 21:04:35 -0600

    feat(home): add contact links
    github and email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ export default function HomePage() {
     <section>
       <h1 className="big-heading">phaedrus</h1>
       <p>software engineer, general tinkerer</p>
+      <div style={{ marginTop: '1rem', display: 'flex', gap: '1rem', flexDirection: "row" }}>
+        <a href="https://github.com/phrazzld" target="_blank" rel="noreferrer" className="content-link secondary-link">
+          github
+        </a>
+        <a href="mailto:phaedrus.raznikov@pm.me" target="_blank" rel="noreferrer" className="content-link secondary-link">
+          email
+        </a>
+      </div>
       <section
         style={{
           minHeight: '50vh',
